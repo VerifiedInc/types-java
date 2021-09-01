df39020df903c342915c43708a226aa2e008d73b
Ray Smets <rayjsmets@gmail.com>
2021-08-31 17:16:18 -0700

    [UpdateCredentialStatus] types. Status enum updated to use lowercase for native json conversion. (#4)
    * [UpdateCredentialStatus] types.
    
    * [docs]

--- src/main/java/id/unum/enums/CredentialStatusOptions.java
+++ src/main/java/id/unum/enums/CredentialStatusOptions.java
@@ -5,8 +5,12 @@ import lombok.Getter;
 
 @AllArgsConstructor
 public enum CredentialStatusOptions {
-    VALID("VALID"),
-    REVOKED("REVOKED");
+    /**
+     * Note opting to go against convention here and use lowercase for the sake of native json conversion which is
+     * all lowercase values.
+     */
+    valid("valid"),
+    revoked("revoked");
 
     @Getter
     private String value;
