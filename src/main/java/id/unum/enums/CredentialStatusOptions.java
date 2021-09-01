98f7c038afd02857a2ead95d378d3b627bce73f4
Ray Smets <rayjsmets@gmail.com>
2021-08-31 17:16:01 -0700

    [docs]

--- src/main/java/id/unum/enums/CredentialStatusOptions.java
+++ src/main/java/id/unum/enums/CredentialStatusOptions.java
@@ -5,6 +5,10 @@ import lombok.Getter;
 
 @AllArgsConstructor
 public enum CredentialStatusOptions {
+    /**
+     * Note opting to go against convention here and use lowercase for the sake of native json conversion which is
+     * all lowercase values.
+     */
     valid("valid"),
     revoked("revoked");
 
