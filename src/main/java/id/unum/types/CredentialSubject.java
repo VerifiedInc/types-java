2fe35ded1e16ad671b31003e92f84d8fd9c845ae
Ray Smets <rayjsmets@gmail.com>
2021-10-11 14:01:21 -0500

    wip

--- src/main/java/id/unum/types/CredentialSubject.java
+++ src/main/java/id/unum/types/CredentialSubject.java
@@ -6,16 +6,40 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.HashMap;
+import java.util.Map;
 
 @Data
 @NoArgsConstructor
-public class CredentialSubject extends HashMap<String, String> {
+//public class CredentialSubject extends HashMap<String, JsonObject> {
+public class CredentialSubject<T> {
+    private Map<String, T> map = new HashMap<>();
+
+    public T getObject(final String key) {
+        return map.get(key);
+    }
+
     public CredentialSubject(String id) {
-        super();
-        super.put("id", id);
+        map.put("id", (T) id);
     }
 
     public String getId() {
-        return super.get("id");
+        return (String) map.get("id");
     }
 }
+
+//@Data
+//@NoArgsConstructor
+//public class CredentialSubject extends HashMap<String, JsonObject> {
+//    String id;
+//
+//    public CredentialSubject(String id) {
+//        super();
+////        super.put("id", id);
+//        this.id = id;
+//    }
+//
+//    public String getId() {
+////        return super.get("id").getAsString();
+//        return id;
+//    }
+//}
